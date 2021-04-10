5089c0e236f4c6ccb598bf549852fd189fb1e6e5
Steve Cordero <>
2021-04-10 04:53:46 +0000

    added JPanel colors

--- Main.java
+++ Main.java
@@ -12,7 +12,8 @@ public class Main
     frame1.setSize(600,1000);
 
     //---------------------Panels-------------------
-    JPanel stats = new JPanel(new GridLayout(1,4,20,20));
+    JPanel stats = new JPanel(new GridLayout(1,4,20,20));//Jpanel to hold the stats
+    JPanel colors = new JPanel(new GridLayout(3,2,15,20));//JPanel to hold the color to indicate player condition within the stats JPanel
 
     //--------------------Labels--------------------
     JLabel playerInfo = new JLabel("<html>Character Info<br/>Name:null  <br/> Height:null<br/>Age:null<br/>Status:Alive<br/>Journey: Incomplete<html>");
@@ -22,13 +23,16 @@ public class Main
     JLabel animalStats = new JLabel("<html>Animal Stats<br/>Health:<br/>100<br/>Hunger:<br/>100<br/><br/><br/>");
 
     //------------------Images------------------------
-    ImageIcon greenImage = new ImageIcon(new ImageIcon("green.png").getImage().getScaledInstance(100,100, Image.SCALE_DEFAULT), "Stable");
+    ImageIcon greenImage = new ImageIcon(new ImageIcon("green.png").getImage().getScaledInstance(150,20, Image.SCALE_DEFAULT), "Stable");
     JLabel greenLabelPic = new JLabel(greenImage);
+
+    
 
     stats.add(playerInfo);
     stats.add(playerStats); 
     stats.add(animalStats);
-    stats.add(greenLabelPic);
+    stats.add(colors);
+    colors.add(greenLabelPic);
 
     frame1.getContentPane().add(BorderLayout.NORTH, stats);
 
