4bf50160d5a7697d191f6b52bb412c9b07dd9f3c
Steve Cordero <>
2021-04-10 04:44:34 +0000

    Added green.png/"ImageIcon" greenImage with JLabel "greenFit"

--- Main.java
+++ Main.java
@@ -9,10 +9,10 @@ public class Main
   {
     JFrame frame1 = new JFrame("Time Adventure");
     frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame1.setSize(500,500);
+    frame1.setSize(600,1000);
 
     //---------------------Panels-------------------
-    JPanel stats = new JPanel(new GridLayout(1,4,40,40));
+    JPanel stats = new JPanel(new GridLayout(1,4,20,20));
 
     //--------------------Labels--------------------
     JLabel playerInfo = new JLabel("<html>Character Info<br/>Name:null  <br/> Height:null<br/>Age:null<br/>Status:Alive<br/>Journey: Incomplete<html>");
@@ -22,15 +22,15 @@ public class Main
     JLabel animalStats = new JLabel("<html>Animal Stats<br/>Health:<br/>100<br/>Hunger:<br/>100<br/><br/><br/>");
 
     //------------------Images------------------------
-
-
-
+    ImageIcon greenImage = new ImageIcon(new ImageIcon("green.png").getImage().getScaledInstance(100,100, Image.SCALE_DEFAULT), "Stable");
+    JLabel greenLabelPic = new JLabel(greenImage);
 
     stats.add(playerInfo);
     stats.add(playerStats); 
     stats.add(animalStats);
+    stats.add(greenLabelPic);
 
-    frame1.getContentPane().add(BorderLayout.NORTH,stats);
+    frame1.getContentPane().add(BorderLayout.NORTH, stats);
 
     frame1.setVisible(true);
   }
