4e5980f9f4bebbf0a6abc62cefd687e8c353333f
Steve Cordero <>
2021-04-10 03:54:32 +0000

    added animalStats label

--- Main.java
+++ Main.java
@@ -12,18 +12,23 @@ public class Main
     frame1.setSize(500,500);
 
     //---------------------Panels-------------------
-    JPanel stats = new JPanel(new GridLayout(1,4,20,20));
+    JPanel stats = new JPanel(new GridLayout(1,4,40,40));
 
     //--------------------Labels--------------------
-    JLabel playerInfo = new JLabel("<html>Name:null  <br/> Height:null<br/>Age:null<br/>Status:Alive<br/>Journey: Incomplete<html>");
+    JLabel playerInfo = new JLabel("<html>Character Info<br/>Name:null  <br/> Height:null<br/>Age:null<br/>Status:Alive<br/>Journey: Incomplete<html>");
 
-    JLabel playerStats = new JLabel("<html>Character Stats<br/>Health:<br/>     100<br/>Hunger:<br/>     100");
+    JLabel playerStats = new JLabel("<html>Character Stats<br/>Health:<br/>100<br/>Hunger:<br/>100<br/><br/><br/><html>");
+
+    JLabel animalStats = new JLabel("<html>Animal Stats<br/>Health:<br/>100<br/>Hunger:<br/>100<br/><br/><br/>");
+
+    //------------------Images------------------------
 
 
 
 
     stats.add(playerInfo);
     stats.add(playerStats); 
+    stats.add(animalStats);
 
     frame1.getContentPane().add(BorderLayout.NORTH,stats);
 
